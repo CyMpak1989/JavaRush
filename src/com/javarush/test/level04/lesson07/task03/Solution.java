998e2a68f2551d7f63a98742da962689c6f59a6e
CyMpak <cympak2009@mail.ru>
2016-11-11 15:45:54 +0300

    Перешли на 5 уровень

---
+++ src/com/javarush/test/level04/lesson07/task03/Solution.java
@@ -0,0 +1,36 @@
+package com.javarush.test.level04.lesson07.task03;
+
+/* Положительные числа
+Ввести с клавиатуры три целых числа. Вывести на экран количество положительных чисел в исходном наборе.
+Пример для чисел -4 6 6:
+2
+Пример для чисел -6 -6 -3:
+0
+*/
+
+import java.io.*;
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int num1 = Integer.parseInt(reader.readLine());
+        int num2 = Integer.parseInt(reader.readLine());
+        int num3 = Integer.parseInt(reader.readLine());
+        int count = 0;
+        if (num1 > 0)
+        {
+            count++;
+        }
+        if (num2 > 0)
+        {
+            count++;
+        }
+        if (num3 > 0)
+        {
+            count++;
+        }
+        System.out.print(count);
+    }
+}
