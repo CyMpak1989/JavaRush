654e7e93240f3adabbf7dbfb8da8292bea2eb967
CyMpak <cympak2009@mail.ru>
2016-11-14 21:54:10 +0300

    8 уровень

---
+++ src/com/javarush/test/level07/lesson12/bonus03/Solution.java
@@ -0,0 +1,42 @@
+package com.javarush.test.level07.lesson12.bonus03;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+/* Задача по алгоритмам
+Задача: Написать программу, которая вводит с клавиатуры 20 чисел и выводит их в убывающем порядке.
+*/
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int[] array = new int[20];
+        for (int i = 0; i < 20; i++)
+        {
+            array[i] = Integer.parseInt(reader.readLine());
+        }
+
+        sort(array);
+
+        for (int x : array)
+        {
+            System.out.println(x);
+        }
+    }
+
+    public static void sort(int[] array)
+    {
+        Arrays.sort(array);
+
+        int[] array1 = new int[20];
+        System.arraycopy(array, 0, array1, 0, 20);
+
+        for (int i = 0; i < array1.length; i++){
+            array[i] = array1[19 - i];
+        }
+    }
+}
