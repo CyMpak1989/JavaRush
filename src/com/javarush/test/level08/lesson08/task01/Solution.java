78c7085f10a75a4ff7016c95d0c978899f960da2
CyMpak <cympak2009@mail.ru>
2016-11-15 15:50:00 +0300

    Пора домой

--- src/com/javarush/test/level08/lesson08/task01/Solution.java
+++ src/com/javarush/test/level08/lesson08/task01/Solution.java
@@ -11,7 +11,11 @@ public class Solution
 {
     public static HashSet<String> createSet()
     {
-        //напишите тут ваш код
-
+        HashSet<String> strings = new HashSet<>();
+        for (int i = 0; i < 20; i++)
+        {
+            strings.add("Лось " + i);
+        }
+        return strings;
     }
 }
