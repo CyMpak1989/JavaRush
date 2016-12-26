34bca9710bbc879f2103fb379237cdf67afef833
CyMpak <cympak2009@mail.ru>
2016-12-27 00:12:03 +0300

    Конец 15 уровня

---
+++ src/com/javarush/test/level15/lesson12/home07/Constants.java
@@ -0,0 +1,5 @@
+package com.javarush.test.level15.lesson12.home07;
+
+public class Constants {
+    public static String FILE_NAME = "C:\\Users\\CyMpak\\Documents\\JavaRush\\src\\com\\javarush\\test\\level15\\lesson12\\home07\\Test.txt";
+}
